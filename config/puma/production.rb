a9e18c64cd04c6216db0a30aec48300e657f7d0a
udf <hellpl4y@gmail.com>
2022-11-21 15:27:36 +0500

    wip

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -3,18 +3,18 @@
 rails_env = 'production'
 environment rails_env
 
-app_dir = '/var/www/testapp/current'
+app_dir = '/var/www/testapp'
 
-bind "unix://#{app_dir}/puma.sock"
-pidfile "#{app_dir}/puma.pid"
-state_path "#{app_dir}/puma.state"
-directory "#{app_dir}/"
+bind "unix://#{app_dir}/shared/puma.sock"
+pidfile "#{app_dir}/shared/puma.pid"
+state_path "#{app_dir}/shared/puma.state"
+directory "#{app_dir}/current/"
 
-stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
+stdout_redirect "#{app_dir}/shared/log/puma.stdout.log", "#{app_dir}/shared/log/puma.stderr.log", true
 
 workers 2
 threads 1, 2
 
-activate_control_app "unix://#{app_dir}/pumactl.sock"
+activate_control_app "unix://#{app_dir}/shared/pumactl.sock"
 
 prune_bundler
